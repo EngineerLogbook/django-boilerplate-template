1d38383286a97485baf50946fff615f8de1488ff
Vividh Mariy <vmariya_be19@thapar.edu>
2020-05-24 05:25:49 +0530

    Deleted unnecessary files from the core_management app

--- src/boilerplate/settings/base_settings.py
+++ src/boilerplate/settings/base_settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core_management.apps.CoreManagementConfig',
+    'core_management',
     'user_management.apps.UserManagementConfig',
     'landingpage.apps.LandingpageConfig',
     'django.contrib.sites',
